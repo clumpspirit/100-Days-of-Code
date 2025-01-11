5055df144e97606bbb8f604512918523d6e623e7
clumpspirit <edindzilic@gmail.com>
2025-01-10 20:00:45 -0700

    challenge: day 1

---
+++ day1-band-generator/bandname.py
@@ -0,0 +1,7 @@
+print("Hi there. Welcome to the Band Name Generator\n")
+
+city_name = input("In which city did you grow up in?\n")
+
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name is " + city_name + " " + pet_name)
---
+++ task.py
@@ -0,0 +1,7 @@
+# print(len(input("What is your name?\n")))
+
+username = input("What is your name?\n")
+
+length = len(username)
+
+print(length)
